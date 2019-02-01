e205e94b4d6245ae3be1e7536ce6e43ccdd3cdd5
Tom <syntraprogrammeurs@gmail.com>
2019-02-01 08:43:55 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,254 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col col-6">
+          <div class="card">
+            <h4 class="card-header bg-dark text-white">
+              Dynamic transitions
+            </h4>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label for="chosenTransition">Select transition</label>
+                  <select
+                          id="chosenTransition"
+                          v-model="chosenTransition"
+                          class="form-control"
+                  >
+                    <option
+                            v-for="transition in transitions"
+                            :value="transition"
+                    >
+                      {{ transition | ucfirst }}
+                    </option>
+                  </select>
+                </div>
+              </form>
+              <button
+                      class="btn btn-primary"
+                      @click="toggle"
+              >
+                Toggle transition
+              </button>
+              <hr />
+              <transition :name="chosenTransition">
+                <div
+                        class="alert alert-success"
+                        v-if="isActive"
+                >
+                  Alert!
+                </div>
+              </transition>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        /**
+         * The name of the application.
+         */
+        name: 'my-app',
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+        /**
+         * The data the Vue instance can use.
+         *
+         * @return {Object} The view model data.
+         */
+        data() {
+            return {
+                isActive: true,
+                chosenTransition: 'bounce',
+                transitions: [
+                    'bounce',
+                    'fade',
+                    'flip-x',
+                    'flip-y',
+                    'light-speed',
+                    'rotate',
+                    'zoom',
+                ],
+            };
+        },
+
+      
+        /**
+         * The methods that are available on this Vue instance.
+         */
+        methods: {
+            /**
+             * Method used to toggle the isActive-property.
+             */
+            toggle() {
+                this.isActive = !this.isActive;
+            },
+        },
+
+        /**
+         * The filters that are available on this Vue instance.
+         */
+        filters: {
+            /**
+             * Will uppercase the first character of the word.
+             *
+             * @param {string} value The value to be uppercased.
+             *
+             * @return {string} The uppercased value.
+             */
+            ucfirst(value) {
+                if (!value) {
+                    return '';
+                }
+
+                return value.charAt(0).toUpperCase() + value.slice(1);
+            }
+        }
+    };
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .bounce-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: bounceIn;
+  }
+
+  @-webkit-keyframes bounceIn {
+    0% {
+      -webkit-transform: translateY(-500px);
+      transform: translateY(-500px);
+      -webkit-animation-timing-function: ease-in;
+      animation-timing-function: ease-in;
+      opacity: 0;
+    }
+    38% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+      opacity: 1;
+    }
+    55% {
+      -webkit-transform: translateY(-65px);
+      transform: translateY(-65px);
+      -webkit-animation-timing-function: ease-in;
+      animation-timing-function: ease-in;
+    }
+    72% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+    }
+    81% {
+      -webkit-transform: translateY(-28px);
+      transform: translateY(-28px);
+      -webkit-animation-timing-function: ease-in;
+      animation-timing-function: ease-in;
+    }
+    90% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+    }
+    95% {
+      -webkit-transform: translateY(-8px);
+      transform: translateY(-8px);
+      -webkit-animation-timing-function: ease-in;
+      animation-timing-function: ease-in;
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+    }
+  }
+  
+  .bounce-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: bounceOut;
+  }
+  
+  .fade-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: fadeIn;
+  }
+  
+  .fade-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: fadeOut;
+  }
+  
+  .flip-x-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: flipInX;
+  }
+  
+  .flip-x-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: flipOutX;
+  }
+  
+  .flip-y-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: flipInY;
+  }
+  
+  .flip-y-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: flipOutY;
+  }
+  
+  .light-speed-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: lightSpeedIn;
+  }
+  
+  .light-speed-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: lightSpeedOut;
+  }
+  
+  .rotate-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: rotateIn;
+  }
+  
+  .rotate-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: rotateOut;
+  }
+  
+  .zoom-enter-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: zoomIn;
+  }
+  
+  .zoom-leave-active {
+    animation-duration: .5s;
+    animation-fill-mode: both;
+    animation-name: zoomOut;
+  }
 </style>
